26abb3c3aa6f5cfbd5d05c1d7b1497bd0cd1ca1f
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2021-02-04 11:12:16 -0500

    I finished visually the login page

--- lib/src/screens/login_page.dart
+++ lib/src/screens/login_page.dart
@@ -25,45 +25,72 @@ class _LoginPageState extends State<LoginPage> {
               height: 200,
             ),
           ),
-          Center(
-            child: Card(
-              margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'User'),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'Password'),
-                      obscureText: true,
-                    ),
-                    RaisedButton(
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      onPressed: () => _login(context),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Log in'),
-                          if (_loading)
-                            Container(
-                              height: 20,
-                              width: 20,
-                              margin: const EdgeInsets.only(left: 20),
-                              child: CircularProgressIndicator(),
+          Transform.translate(
+            offset: Offset(0, -20),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 260),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 35, vertical: 20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'User'),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Password'),
+                          obscureText: true,
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        RaisedButton(
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          onPressed: () => _login(context),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Log in'),
+                              if (_loading)
+                                Container(
+                                  height: 20,
+                                  width: 20,
+                                  margin: const EdgeInsets.only(left: 20),
+                                  child: CircularProgressIndicator(),
+                                )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Are you registered?'),
+                            FlatButton(
+                              textColor: Theme.of(context).primaryColor,
+                              child: Text('Register'),
+                              onPressed: () {
+                                _showRegister(context);
+                              },
                             )
-                        ],
-                      ),
-                    )
-                  ],
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -79,5 +106,9 @@ class _LoginPageState extends State<LoginPage> {
         this._loading = true;
       });
     }
+  }
+
+  void _showRegister(BuildContext context) {
+    Navigator.of(context).pushNamed('/register');
   }
 }
